b277eb46117ef590c2866571945fc68866e1d04f
Donavan <cddonav@3c22fb076860.ant.amazon.com>
2020-09-23 12:08:04 -0700

    initial commit

--- cdk_demo/cdk_demo_stack.py
+++ cdk_demo/cdk_demo_stack.py
@@ -3,6 +3,9 @@ from aws_cdk import (
     aws_sqs as sqs,
     aws_sns as sns,
     aws_sns_subscriptions as subs,
+    aws_lambda as _lambda,
+    aws_dynamodb as ddb,
+    aws_lambda_event_sources as lambda_event_source,
     core
 )
 
@@ -12,13 +15,39 @@ class CdkDemoStack(core.Stack):
     def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
-        queue = sqs.Queue(
-            self, "CdkDemoQueue",
+        # Creates Queue
+        requestqueue = sqs.Queue(
+            self, "RequestsQueue",
             visibility_timeout=core.Duration.seconds(300),
         )
 
-        topic = sns.Topic(
-            self, "CdkDemoTopic"
+        # Create DDB Table
+        requests_table = ddb.Table(
+            self, "requests_table",
+            partition_key=ddb.Attribute(
+                name="id",
+                type=ddb.AttributeType.STRING
+            )
         )
 
-        topic.add_subscription(subs.SqsSubscription(queue))
+        # Defines an AWS Lambda resource
+        hello_lambda = _lambda.Function(
+            self, 'HelloHandler',
+            runtime=_lambda.Runtime.PYTHON_3_7,
+            code=_lambda.Code.asset('lambda'),
+            handler='hello.handler',
+        )
+
+        # Adds DN Table as env
+        hello_lambda.add_environment("TABLE_NAME", requests_table.table_name)
+
+        # grant permission to lambda to write to demo table
+        requests_table.grant_write_data(hello_lambda)
+
+        # SQS Event Source for Lambda
+        sqs_event_source = lambda_event_source.SqsEventSource(requestqueue)
+
+        # SQS event source to Function
+        hello_lambda.add_event_source(sqs_event_source)
+
+        
